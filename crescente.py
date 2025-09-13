64e9fae19dd60acd33354913d05f22537987b8ec
Camila Marques <milamazim@gmail.com>
2025-09-13 14:39:50 -0300

    Exercicio crescente

---
+++ crescente.py
@@ -0,0 +1,13 @@
+print("Digite dois números: ")
+x = input()
+y = input()
+
+while x != y:
+  if (x > y):
+    print("DECRESCENTE!")
+  else:
+    print("CRESCENTE!")
+ 
+  print("Digite outros dois números: ")
+  x = input()
+  y = input()
