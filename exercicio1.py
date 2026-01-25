e2a837cc79edc1cb92fd8c7850d04b1e7345cc9d
Camila Marques <milamazim@gmail.com>
2026-01-25 13:00:37 +0000

    criada primeira versao funcional do crud de usuarios

--- exercicio1.py
+++ exercicio1.py
@@ -1,67 +1,104 @@
+LINHA = 50
 usuarios = []
 
 def criar_usuario():
-    registro = []    
-    registro.append(input('Usuário: '))
-    registro.append(input('E-mail: '))
-    usuarios.append(registro)
-    print('-----------------------------------------------')
+    usuario = {
+        "id": len(usuarios) + 1,
+        "nome": input("Usuário: "),
+        "email": input("E-mail: ")
+    }
+    usuarios.append(usuario)
+    print("Usuário criado com sucesso!")
+    print("-" * LINHA)
+
 
 def listar_usuario():
-    print('Usuarios cadastrados: ')    
-    for x in usuarios:
-        print(f'ID: {usuarios.index(x)}')
-        print(f'Nome: {x[0]}')
-        print(f'E-mail: {x[1]}')
-        print('-----------------------------------------------')
+    if not usuarios:
+        print("Nenhum usuário cadastrado")
+        print("-" * LINHA)
+    else:
+        for user in usuarios:
+            print(f"ID: {user['id']}")
+            print(f"Nome: {user['nome']}")
+            print(f"Email: {user['email']}")
+            print("-" * LINHA)
+
 
 def atualizar_usuario():
-    usuario = input('Atualizar nome de qual usuário? ')    
-    
-    for x in usuarios:        
-        if (x[0] == usuario):
-            update = input('Atualizar nome: ')
-            indice = usuarios.index(x)
-            usuarios[indice][0] = update   
-        
+    while True:
+        try:
+            user_id = int(input("Digite o ID do usuário: "))
+            print("-" * LINHA)
+            break
+        except ValueError:
+            print("Digite apenas números.")
+            print("-" * LINHA)
+
+    for user in usuarios:
+        if user["id"] == user_id:
+            user["nome"] = input("Novo nome: ")
+            user["email"] = input("Novo email: ")
+            print("Usuário atualizado!")
+            print("-" * LINHA)
+            return
+
+    print("Usuário não encontrado")
+    print("-" * LINHA)
+
+
 def remover_usuario():
-    usuario = input('Informe o usuário a ser removido: ')
+    while True:
+        try:
+            user_id = int(input("Digite o ID do usuário: "))
+            print("-" * LINHA)
+            break
+        except ValueError:
+            print("Digite apenas números.")
+            print("-" * LINHA)
 
-    for x in usuarios:        
-        if (x[0] == usuario):            
-            indice = usuarios.index(x)
-            usuarios.remove(usuarios[indice])            
-  
+    for user in usuarios:
+        if user["id"] == user_id:
+            usuarios.remove(user)
+            print("Usuário excluído!")
+            print("-" * LINHA)
+            return
+
+    print("Usuário não encontrado")
+    print("-" * LINHA)
+
+
 def menu():
-    print('Digite o número da opção desejada: \n' +
-        '----------------------------------------------- \n' + 
-        '(1) Criar usuário \n' +
-        '(2) Listar usuário \n' +
-        '(3) Atualizar usuário \n' +
-        '(4) Remover usuário \n' +
-        '(0) Sair\n' + 
-        '-----------------------------------------------')
+    while True:
+        print('Digite o número da opção desejada:')
+        print('-' * LINHA)
+        print('(1) Criar usuário')
+        print('(2) Listar usuário')
+        print('(3) Atualizar usuário')
+        print('(4) Remover usuário')
+        print('(0) Sair')
+        print('-' * LINHA)
 
-    opcao = int(input('Opção: '))
-    print('-----------------------------------------------')
-   
-    match opcao:
-        case 1:
-            criar_usuario()
-            menu()
-        case 2:            
-            listar_usuario()
-            menu()
-        case 3:
-            atualizar_usuario()
-            menu()
-        case 4:
-            remover_usuario()
-            menu()
-        case 0:
-            print('Programa finalizado')
-        case _: # default
-            print('Opção inexistente')
-            menu()            
+        try:
+            opcao = int(input('Opção: '))
+            print("-" * LINHA)
+        except ValueError:
+            print("Digite apenas números.")
+            print("-" * LINHA)
+            continue
+
+        match opcao:
+            case 1:
+                criar_usuario()
+            case 2:
+                listar_usuario()
+            case 3:
+                atualizar_usuario()
+            case 4:
+                remover_usuario()
+            case 0:
+                print('Programa finalizado')
+                break
+            case _:
+                print('Opção inexistente')
 
 menu()
